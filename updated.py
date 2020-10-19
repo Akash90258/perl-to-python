50959fab6b73a66f3c306e91ad8e4ca90d2b86f2
Akash90258 <42032895+Akash90258@users.noreply.github.com>
2020-10-19 11:30:18 +0530

    Update updated.py

--- updated.py
+++ updated.py
@@ -1,6 +1,13 @@
 import os
 # print(os.path.isdir("/home/el"))
 
+
+def read_file(file_name):
+    f = open(file_name,"r")
+    file_data = f.read()
+    f.close()
+    return file_data
+
 def set_users_conf():
     """Read pma_bkp_tracker.conf file and create dict."""
     users_details = {}
@@ -14,6 +21,8 @@ def set_users_conf():
         users_details[key.strip()] = values.split(',')
     print(users_details)
     return users_details
+
+from datetime import datetime
 
 
 def read_opco_dir():
@@ -52,11 +61,45 @@ def read_inp(inp_dir_path, user, inp_dir):
         inp_files = os.listdir(inp_dir_path)
     else:
         print("Can't open the current directory")
-    print(inp_files)
+    # print(inp_files)
+
+    inp_files2 = []
+    for file in inp_files:
+        if '.inp' in file:
+            inp_files2.append(file)
+
+    if user not in parsed_hash:
+        parsed_hash[user] = {}
+    if host not in parsed_hash[user]:
+        parsed_hash[user][host] = {}
+    if ip not in parsed_hash[user][host]:
+        parsed_hash[user][host][ip] = {}
+
+    if len(inp_files2) == 0:
+        for array_ref1 in array_ref:
+            parsed_hash[user][host][ip][array_ref1] = issue1
+    else:
+        nedate = read_file(inp_dir_path+"/nedate.inp")
+        if curr_date2 in nedate:
+            for array_ref1 in array_ref:
+                parsed_hash[user][host][ip][array_ref1] = 'N/A'
+            print("yes==============")
+        else:
+            for array_ref1 in array_ref:
+                parsed_hash[user][host][ip][array_ref1] = issue1
+
+
+    print(parsed_hash)
+    # print(inp_dir_path, user, inp_dir)
     print("==================")
 
 
 if __name__ == '__main__':
     global users_details
+    global parsed_hash
+    curr_date2 = str(datetime.today().strftime('%Y%m%d'))
+
+    issue1 = 'Connectivity/Password Issue'
+    parsed_hash = {}
     users_details = set_users_conf()
     read_opco_dir()
