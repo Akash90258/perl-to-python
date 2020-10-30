24ba4eee0de19af102cb84895be771884daa29d1
Ubuntu <ubuntu@ip-172-31-14-127.us-west-2.compute.internal>
2020-10-30 11:36:49 +0000

    Uploading after testing and discussed with pankaj sir

---
+++ congo/create_excel.py
@@ -0,0 +1,110 @@
+import logging
+from datetime import date
+
+import openpyxl
+import pandas as pd
+from utils import *
+
+
+print("Process Started")
+# Initialsing Loggine module
+log_file_path = "./logs/logs_{}.log".format(str(date.today()))
+logging.basicConfig(
+    level=logging.INFO,
+    filename=log_file_path
+)
+logging.info("Starting Automation...")
+
+# Loading config.ini file into a dictionary
+logging.info("Loading Config.ini")
+config_data = load_config_ini()
+
+# mapping for dynamic function calling
+logging.info("Loading Dynamic Function mapping")
+mapping = get_dynamic_function_dict()
+
+# initialising variable from config.ini
+logging.info("Loading Variables from config.ini ")
+Base_path = config_data['base_dir_inp_files']
+Template_file = config_data['template_file']
+Output_File = config_data['output_file_path'] + \
+              config_data['output_file_prefix'] + \
+              str(date.today()) + ".xlsx"
+pma_nw_file_path = config_data['pma_nw_file_path']
+
+try:
+    # reading pma_nw_final.conf and creating dictionary
+    logging.info("Loading Configations from pma_nw_file_path.conf")
+    updated_pma_dict = pma_nw_node_dict(pma_nw_file_path)
+
+    # Creating File which we will have in Output
+    logging.info("Creating Object for outptu Excel ")
+    writer = pd.ExcelWriter(Output_File, engine='openpyxl')
+    book = openpyxl.load_workbook(Template_file)
+    writer.book = book
+    writer.sheets = dict((ws.title, ws) for ws in book.worksheets)
+
+    # Loading template in pandas dataframe
+    logging.info("Reading Input Template file")
+    template_to_df_map = pd.read_excel(Template_file, sheet_name=None)
+
+    # Generating Dictionary after reading and parsing node types directories
+    # based on nedate and its corresponding Inp file
+    logging.info("Creating dictinary of all node types after parsing ini files")
+    parsed_hash = main()
+
+    # Iterating each node type for Update individual output sheet
+    for key, values in parsed_hash.items():
+        opco = key
+
+        # Continue if opco is commented in pma_nw-final.cong
+        if opco.lower() not in updated_pma_dict.keys():
+            continue
+
+        # calling nodes_iterators functions dynamically for its respective nodetype
+        logging.info("Calling custom function for OPCO : {}".format(opco))
+        custom_function = mapping['nodes_iterator_' + opco.lower()]
+        final_data = custom_function(values, updated_pma_dict[opco.lower()])
+
+        logging.info("Writting data for OPCO : {}".format(opco))
+        for row in final_data:
+            # removing the empty row from excel sheet
+            try:
+                template_to_df_map[opco.upper()].dropna(subset=['Node IP'], inplace=True)
+            except:
+                pass
+            template_to_df_map[opco.upper()] = template_to_df_map[opco.upper()]\
+                .append(row, ignore_index=True)
+
+        # Writing Dataframe to its respective node type sheet in output excel file
+        template_to_df_map[opco.upper()].to_excel(writer, opco.upper(), index=False)
+
+        # create excel Summary sheet in outfile
+        writer = excel_summary_writer(template_to_df_map, writer)
+
+    # Flattening Json and return a list of nodes of NotDone cases for Logs sheet.
+    logging.info("Flatting Dataframe")
+    log_rows = json_flatten_for_logs(parsed_hash, updated_pma_dict, Base_path)
+
+    logging.info("Writting Data to Logs sheet in output excel file")
+
+    if len(log_rows)>0:
+        template_to_df_map['Logs'] = template_to_df_map['Logs']\
+        .append(log_rows, ignore_index=True)
+
+        template_to_df_map['Logs'].to_excel(
+            writer, 'Logs', index=False, startrow=1, startcol=0, header=False
+         )
+
+    # print("===================================")
+    # print(template_to_df_map['AIR'])
+    # print("===================================")
+    writer.save()
+    logging.info("Sending mail")
+    Send_Email_SMTP(Output_File)
+except Exception as error:
+    logging.error("Error Occured: {}".format(error))
+    logging.info("Sending Failier mail")
+    Send_Email_SMTP(log_file_path)
+logging.info("Automation Ended")
+print("Process Ended")
--- congo/updated.py
+++ congo/updated.py
@@ -1,6 +1,6 @@
-from datetime import datetime
 import os
 import re
+from datetime import datetime
 
 try:
     from configparser import ConfigParser
@@ -9,6 +9,7 @@ except ImportError:
 
 
 def load_config_ini():
+    # Loading config.ini file into a dictionary
     config = ConfigParser()
     config.read('final_config.ini')
     config_data = dict(config.items('section1'))
@@ -16,16 +17,16 @@ def load_config_ini():
 
 
 def read_file(file_name):
+    # Reading a file and returning its data
     f = open(file_name, "r")
     file_data = f.read()
     f.close()
-    return file_data
+    return file_data  # .encode('utf-8')
 
 
 def set_users_conf():
-    """Read pma_bkp_tracker.conf file and create dict."""
+    # Read pma_bkp_tracker.conf file and create dict.
     users_details = {}
-    # conf_file = "conf/pma_bkp_tracker.conf"
     conf_file = config_data['pma_bkup_tracker']
     fh = open(conf_file, "r")
     data = fh.readlines()
@@ -34,11 +35,11 @@ def set_users_conf():
         line = line.strip()
         key, values = line.split("=")
         users_details[key.strip()] = values.split(',')
-    print(users_details)
     return users_details
 
 
 def read_opco_dir():
+    # Reading All node type directories from datasrc folder
     basedir = config_data['base_dir_inp_files']
     child_dir = os.listdir(basedir)
     for inner_dir in child_dir:
@@ -100,24 +101,27 @@ def read_inp(inp_dir_path, user, inp_dir):
                 inp_file = "{}/{}".format(inp_dir_path, inp_name)
                 file_data = read_file(inp_file)
 
-                ##----------------- AIR Backup Check -----------------##
+                ##-- AIR Backup Check --##
                 if 'air' in user_l:
                     status = ''
                     fail_reason = ''
                     if 'backup.inp' in inp_name:
-                        status, fail_reason = tape(file_data, curr_date, curr_date5, inp_dir_path)
+                        status, fail_reason = tape(
+                            file_data, curr_date, curr_date5, inp_dir_path
+                        )
                         parsed_hash[user][host][ip][inp_name] = status + "^^" + fail_reason
 
-                ##----------------- OCC tape Check -----------------##
+                ##-- OCC tape Check --##
                 if 'occ' in user_l:
                     status = ''
                     fail_reason = ''
                     if 'fs_occ_backup.inp' in inp_name:
-                        status, fail_reason = tape(file_data, curr_date, curr_date5, inp_dir_path)
+                        status, fail_reason = tape(
+                            file_data, curr_date, curr_date5, inp_dir_path
+                        )
                         parsed_hash[user][host][ip][inp_name] = status + "^^" + fail_reason
 
-                ##----------------- SDP geo-redundancy and tape Check -----------------##
-
+                ##-- SDP geo-redundancy and tape Check --##
                 if 'sdp' in user_l:
                     status = ''
                     fail_reason = ''
@@ -137,10 +141,10 @@ def read_inp(inp_dir_path, user, inp_dir):
                             parsed_hash[user][host][ip]['geo-redundancy.inp'] = 'Fail'
 
                     if 'backup.inp' in inp_name:
+                        status, fail_reason = tape(file_data, curr_date, curr_date5, inp_dir_path)
                         parsed_hash[user][host][ip][inp_name] = status + "^^" + fail_reason
 
-                ##------------ NGVS geo-redundancy, nfs and cassendra Check -----------##
-
+                ##-- NGVS geo-redundancy, nfs and cassendra Check --##
                 if 'ngvs' in user_l:
                     status = ''
                     # if 'fs.inp' in inp_name:
@@ -155,14 +159,14 @@ def read_inp(inp_dir_path, user, inp_dir):
                         status = zoo(file_data, curr_date, curr_date4)
                         parsed_hash[user][host][ip][inp_name] = status
 
-                ##----------------- CCN dbn Check -----------------##
+                ##-- CCN dbn Check --##
                 if 'ccn' in user_l:
                     status = ''
                     if 'dbn_backup.inp' in inp_name:
                         status = dbn(file_data, curr_date3, '')
                         parsed_hash[user][host][ip][inp_name] = status
 
-                ##----------------- MINSAT fs and db Check -----------------##
+                ##-- MINSAT fs and db Check --##
                 if 'minsat' in user_l:
                     status = ''
                     fail_reason = ''
@@ -175,8 +179,7 @@ def read_inp(inp_dir_path, user, inp_dir):
                         status = dbn(file_data, pre_date1, '')
                         parsed_hash[user][host][ip][inp_name] = status
 
-                ##-------- VS tape, nfs, ora, ora_archive and geo-redundancy Check ----------##
-
+                ##-- VS tape, nfs, ora, ora_archive and geo-redundancy Check --##
                 if 'vs' in user_l:
                     status = ''
                     fail_reason = ''
@@ -195,8 +198,7 @@ def read_inp(inp_dir_path, user, inp_dir):
                             if 'month' in row:
                                 cassendra_arr.append(ow.strip())
 
-                ##----------------- EMA proclog and sogconfig Check -----------------##
-
+                ##--EMA proclog and sogconfig Check --##
                 if 'ema' in user_l:
                     status = ''
                     fail_reason = ''
@@ -209,12 +211,7 @@ def read_inp(inp_dir_path, user, inp_dir):
                         status = proclog(file_data, curr_date4, curr_date2)
                         parsed_hash[user][host][ip][inp_name] = status
 
-                ##----------------- NGCRS appfs, archived CDR and oracle database Check -----------------##
-
-                # not adding NGCRS . because not present in congo opco
-
-                ##--------- CRS appfs, archived CDR and oracle database Check,fs backup ---------##
-
+                ##-- CRS appfs, archived CDR and oracle database Check,fs backup --##
                 if 'crs' in user_l:
                     status = ''
                     fail_reason = ''
@@ -226,7 +223,7 @@ def read_inp(inp_dir_path, user, inp_dir):
                         status = appfs(file_data, curr_date2, curr_date3)
                         parsed_hash[user][host][ip][inp_name] = status
 
-            ##------------- geo-redundancy check -------------##
+            ##-- geo-redundancy check --##
             if sdp_geo_check == 2 and parsed_hash[user][host][ip]['geo-redundancy.inp'] == 'N/A':
                 parsed_hash[user][host][ip]['geo-redundancy.inp'] = 'Success'
         else:
@@ -268,7 +265,7 @@ def dbn(data, date1, date2):
     """Used for db_backup.inp"""
     regex = "cfbackup.*?{}.*?log".format(curr_date2)
     status = ''
-    if 'ScheduledBackup' + date1 in data:
+    if 'ScheduledBackup_' + date1 in data:
         status = 'Success'
     elif 'INFO:root:Filesystem backup ended at ' + date2 in data:
         status = 'Success'
@@ -362,31 +359,28 @@ curr_date6 = str(datetime.today().strftime('%Y/%m/%d'))
 curr_date7 = str(datetime.today().strftime('%A, %B %e, %Y'))
 issue1 = 'Connectivity/Password Issue'
 
+# if __name__ == '__main__':
+#     global users_details
+#     global parsed_hash
+#     global config_data
+#     config_data = load_config_ini()
+#     sdp_geo_check = 0
+#     sdp_geo = ''
+#     cassandra_flag = 0
+#     cassendra_arr = []
+#     month_date = str(datetime.today().strftime('%b %e'))
+#     pre_date1 = str(datetime.today().strftime('%Y_%m_%d -d -1 day'))
+#     curr_date = str(datetime.today().strftime('%Y-%m-%d'))
+#     curr_date2 = str(datetime.today().strftime('%Y%m%d'))
+#     curr_date3 = str(datetime.today().strftime('%Y_%m_%d'))
+#     curr_date4 = str(datetime.today().strftime('%y%m%d'))
+#     curr_date5 = str(datetime.today().strftime('%A, %B %d, %Y'))
+#     curr_date6 = str(datetime.today().strftime('%Y/%m/%d'))
+#     curr_date7 = str(datetime.today().strftime('%A, %B %e, %Y'))
 
-
-
-if __name__ == '__main__':
-    global users_details
-    global parsed_hash
-    global config_data
-    config_data = load_config_ini()
-    sdp_geo_check = 0
-    sdp_geo = ''
-    cassandra_flag = 0
-    cassendra_arr = []
-    month_date = str(datetime.today().strftime('%b %e'))
-    pre_date1 = str(datetime.today().strftime('%Y_%m_%d -d -1 day'))
-    curr_date = str(datetime.today().strftime('%Y-%m-%d'))
-    curr_date2 = str(datetime.today().strftime('%Y%m%d'))
-    curr_date3 = str(datetime.today().strftime('%Y_%m_%d'))
-    curr_date4 = str(datetime.today().strftime('%y%m%d'))
-    curr_date5 = str(datetime.today().strftime('%A, %B %d, %Y'))
-    curr_date6 = str(datetime.today().strftime('%Y/%m/%d'))
-    curr_date7 = str(datetime.today().strftime('%A, %B %e, %Y'))
-
-    issue1 = 'Connectivity/Password Issue'
-    parsed_hash = {}
-    users_details = set_users_conf()
-    read_opco_dir()
-    # print("========================================")
-    # print(parsed_hash)
+#     issue1 = 'Connectivity/Password Issue'
+#     parsed_hash = {}
+#     users_details = set_users_conf()
+#     read_opco_dir()
+#     print("========================================")
+#     print(parsed_hash)
